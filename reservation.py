91a2b84e0b32b8b35d52bb221121d6a019ab764d
khyunchoi <khyunchoi@naver.com>
2021-11-26 22:00:04 +0900

    Update reservation

--- reservation.py
+++ reservation.py
@@ -1,35 +1,62 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+import re
 import time
 
-url = 'https://camping.gtdc.or.kr/DZ_reservation/reserCamping_v3.php?xch=reservation&xid=camping_reservation' # 목적 홈페이지 주소
+url = 'https://camping.gtdc.or.kr/DZ_reservation/reserCamping_v3.php?xch=reservation&xid=camping_reservation&step=Areas&sdate=202112' # 목적 홈페이지 주소
 driver_url = 'C:/Users/khyun/OneDrive/바탕 화면/toy_project/camping_reservation/chromedriver.exe' # chromedriver 파일 주소
 
-# 드라이버 실행
-driver = webdriver.Chrome(driver_url)
-driver.get(url)
+def check_time():
+    driver_time = webdriver.Chrome(driver_url)
+    driver_time.get('https://time.navyism.com/?host=camping.gtdc.or.kr') # 네이비즘 캠핑장 주소
 
-# 화면 최대화
-# driver.maximize_window()
+    # 밀리세컨드 체크
+    xpath = "//input[@id='msec_check']"
+    driver_time.find_element(By.XPATH, xpath).click()
 
-# 팝업 제거
-driver.find_element_by_css_selector('.btn-dark').click()
+    while True:
+        xpath = "//div[@id='time_area']"
+        a = driver_time.find_element(By.XPATH, xpath).text
+        xpath = "//div[@id='msec_area']"
+        b = driver_time.find_element(By.XPATH, xpath).text
 
-# 날짜 지정
-xpath = "//button[@value='A:2021-11-28']" 
-driver.find_element_by_xpath(xpath).click()
-time.sleep(0.1)
+        current_time = re.findall('[0-9]+', a)
+        # current_time[3] 시, current_time[4] 분, current_time[5] 초
+        if(current_time[3] == '21' and current_time[4] == '57' and current_time[5] == '20'):
+            msec = re.findall('[0-9]+', b)
 
-# 구역 지정
-driver.find_element_by_css_selector('#camping_zone > button:nth-child(11)').click()
+            if(int(msec[0]) >= 0):
+                # 드라이버 실행
+                driver = webdriver.Chrome(driver_url)
+                driver.get(url)
 
-# 인원 지정
-xpath = "//html/body/div[4]/table/tbody/tr/td[3]/div/div/table[1]/tbody/tr/td[4]/select/option[3]" 
-driver.find_element_by_xpath(xpath).click() 
+                # 화면 최대화
+                # driver.maximize_window()
 
-# 예약 기간
-xpath = "//html/body/div[4]/table/tbody/tr/td[3]/div/div/div[5]/select/option[1]" 
-driver.find_element_by_xpath(xpath).click() 
+                # 팝업 제거
+                driver.find_element(By.CSS_SELECTOR, '.btn-dark').click()
 
-# 다음 단계
-xpath = "//html/body/div[4]/table/tbody/tr/td[3]/div/div/div[6]/button[2]" 
-driver.find_element_by_xpath(xpath).click() 
+                # 날짜 지정
+                xpath = "//button[@value='C:2021-12-26']"
+                driver.find_element(By.XPATH, xpath).click()
+                time.sleep(0.1)
+
+                # 구역 지정
+                xpath = "//html/body/div[4]/table/tbody/tr/td[3]/div/div/div[4]/div/button[1]" 
+                driver.find_element(By.XPATH, xpath).click()
+
+                # 인원 지정
+                xpath = "//html/body/div[4]/table/tbody/tr/td[3]/div/div/table[1]/tbody/tr/td[4]/select/option[3]" 
+                driver.find_element(By.XPATH, xpath).click()
+
+                # 예약 기간
+                xpath = "//html/body/div[4]/table/tbody/tr/td[3]/div/div/div[5]/select/option[1]" 
+                driver.find_element(By.XPATH, xpath).click()
+
+                # 다음 단계
+                xpath = "//html/body/div[4]/table/tbody/tr/td[3]/div/div/div[6]/button[2]" 
+                driver.find_element(By.XPATH, xpath).click()
+                
+
+# 함수 실행
+check_time()
