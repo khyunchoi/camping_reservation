cde17bb016f27207a07396da87779bed86930180
khyunchoi <khyunchoi@naver.com>
2022-01-18 23:41:31 +0900

    Add 220118 TIL

--- reservation.py
+++ reservation.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 import re
 import time
 
-url = 'https://camping.gtdc.or.kr/DZ_reservation/reserCamping_v3.php?xch=reservation&xid=camping_reservation&step=Areas&sdate=202112' # 목적 홈페이지 주소
+url = 'https://camping.gtdc.or.kr/DZ_reservation/reserCamping_v3.php?xch=reservation&xid=camping_reservation&step=Areas&sdate=202202' # 목적 홈페이지 주소
 driver_url = 'C:/Users/khyun/OneDrive/바탕 화면/toy_project/camping_reservation/chromedriver.exe' # chromedriver 파일 주소
 
 def check_time():
@@ -38,23 +38,24 @@ def check_time():
                 driver.find_element(By.CSS_SELECTOR, '.btn-dark').click()
 
                 # 날짜 지정
-                xpath = "//button[@value='D:2021-12-31']"
+                xpath = "//button[@value='A:2022-02-19']"
                 driver.find_element(By.XPATH, xpath).click()
 
+                time.sleep(0.1)
                 # 구역 지정
-                xpath = "//html/body/div[4]/table/tbody/tr/td[3]/div/div/div[4]/div/button[2]" 
-                while len(driver.driver.find_element(By.XPATH, xpath)) < 1:
-                    driver.refresh()
-                    time.sleep(0.07)
-                else:
-                    driver.find_element(By.XPATH, xpath).click()
+                xpath = "//html/body/div[4]/table/tbody/tr/td[3]/div/div/div[4]/div/button[42]" 
+                # while len(driver.driver.find_element(By.XPATH, xpath)) < 1:
+                #     driver.refresh()
+                #     time.sleep(0.1)
+                # else:
+                driver.find_element(By.XPATH, xpath).click()
 
                 # 인원 지정
                 xpath = "//html/body/div[4]/table/tbody/tr/td[3]/div/div/table[1]/tbody/tr/td[4]/select/option[3]" 
                 driver.find_element(By.XPATH, xpath).click()
 
                 # 예약 기간
-                xpath = "//html/body/div[4]/table/tbody/tr/td[3]/div/div/div[5]/select/option[2]" 
+                xpath = "//html/body/div[4]/table/tbody/tr/td[3]/div/div/div[5]/select/option[1]" 
                 driver.find_element(By.XPATH, xpath).click()
 
                 # 다음 단계
